28e48af5db80f6dc6b831a2f3288f1bcd88fb77d
Simonbiker <smacr25@gmail.com>
2021-07-01 08:12:30 +0100

    Adding small note to read me file and removing unwanted comments for ts files.

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+## Developer Notes
+
+Two new folders created profile-settings and service. Inside those folders you will find my soluation to the problem. Nothing should need to be built simply run ng serve.
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
--- src/app/profile-settings/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings/profile-settings.component.ts
@@ -62,20 +62,3 @@ export class ProfileSettingsComponent implements OnInit {
     this.savingErrorMessage = '';
   }
 }
-
-// To do:
-// 1. Load the profile object once the ProfileSettings component is initialized, display the username, and populate the first name into the input field. If the request results in an error, we should automatically fetch it again until we get it. DONE
-
-// 2. While the data is not ready, we should display the label “Loading profile...” and disable the inputs.
-// DONE
-
-// 3. Once the data is ready, clicking the Save button we should make use of the setName() method and update the first name of the user in the profile. While this is happening we should display the “Saving Profile...” label. If the service
-// returns an error, we should display the text "Error!" and the error message returned by the service. This text should be removed as soon as the value in the input changes or the "Save" button is hit again.
-
-// 4. Add a new input field to update the “last name” too. There should be only 1 Save button to update both fields at the same time. This data should be sent to the profile service using the same setName() method.
-
-// 5. Add a new field to the profile object: “email”. This should be included in both the service and component. We want to display this new field in the component, but do not update it manually.
-// In the service we need a new method setUserEmail, similar to setName but to update the "user.email". The email value needs to be set directly from “[user.firstName].[user.lastName]@blueface.com”. Make sure the email is valid,
-// removing any spaces from the first/last name fields.
-// This method should be called automatically after setName() successfully finished. It should emulate a different http request (use a timeout like the other), and can also return an error sometimes. If that happens, the component should
-// display a different error message “Error on email generation”, and the firstName and lastName should be reverted to the initial value (before setName).
